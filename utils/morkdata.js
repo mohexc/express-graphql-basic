8fe163bd746da2759ce3bf11c7258c9d7d7722d4
nanut <galunyu@gmail.com>
2021-04-25 12:59:20 +0700

    [finish] .

---
+++ utils/morkdata.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    id: "23",
+    firstName: 'Bill',
+    age: 20
+  },
+  {
+    id: "47",
+    firstName: 'Samantha',
+    age: 30
+  },
+  {
+    id: "33",
+    firstName: 'Tony',
+    age: 44
+  },
+]
+
+module.exports = {
+  users
+}
